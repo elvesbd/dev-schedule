a0bdbe80cf275254e3fb420a8513acfc977e1f7e
elvesbd <elvesbd@yahoo.com>
2023-10-11 21:03:29 -0300

    test: refactor create id

--- src/core/test/shared/model/base.entity.spec.ts
+++ src/core/test/shared/model/base.entity.spec.ts
@@ -1,15 +1,16 @@
 import { BaseEntity } from '@core/shared/model';
+import { Id } from '@core/shared/values-objects';
 
 class ConcreteEntity extends BaseEntity {
-  constructor(id: string) {
-    super(id);
+  constructor(id: Id) {
+    super(id.getValue);
   }
 }
 
 describe('BaseEntity [model]', () => {
   it('should check if two entities are equal', () => {
-    const id1 = 'b8c3eec4-0b7f-4814-aabd-0e800ef14711';
-    const id2 = 'b8c3eec4-0b7f-4814-aabd-0e800ef14711';
+    const id1 = new Id('b8c3eec4-0b7f-4814-aabd-0e800ef14711');
+    const id2 = new Id('b8c3eec4-0b7f-4814-aabd-0e800ef14711');
     const entity1 = new ConcreteEntity(id1);
     const entity2 = new ConcreteEntity(id2);
 
@@ -17,8 +18,8 @@ describe('BaseEntity [model]', () => {
   });
 
   it('should check if two entities are not equals', () => {
-    const id1 = 'b8c3eec4-0b7f-4814-aabd-0e800ef14711';
-    const id2 = 'b7c3eec4-0b7f-4814-aabd-0e800ef15589';
+    const id1 = new Id('b8c3eec4-0b7f-4814-aabd-0e800ef14711');
+    const id2 = new Id('b7c3eec4-0b7f-4814-aabd-0e800ef15589');
     const entity1 = new ConcreteEntity(id1);
     const entity2 = new ConcreteEntity(id2);
 
