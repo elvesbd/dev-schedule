3bf3e4975c0382738ded404fc943eade4f947a4f
elvesbd <elvesbd@yahoo.com>
2023-10-14 12:18:00 -0300

    feat: create document data builder

---
+++ src/core/test/shared/data-builders/document-data-builder.ts
@@ -0,0 +1,26 @@
+export class DocumentDataBuilder {
+  private value: string = '';
+
+  public static aDocument(): DocumentDataBuilder {
+    return new DocumentDataBuilder();
+  }
+
+  withCpf(): this {
+    this.value = '999.999.999-00';
+    return this;
+  }
+
+  withInvalidCpf(): this {
+    this.value = '99.999.999-00';
+    return this;
+  }
+
+  withCnpj(): this {
+    this.value = '12.345.678/0001-00';
+    return this;
+  }
+
+  public build(): string {
+    return this.value;
+  }
+}
