5c728be9ad2728cba11b8505173c7b461672f54e
elvesbd <elvesbd@yahoo.com>
2023-10-14 15:44:28 -0300

    test: created unit tests for email value object

---
+++ src/core/test/shared/value-objects/email.spec.ts
@@ -0,0 +1,56 @@
+import { Email } from '@core/shared/values-objects';
+import { EmailDataBuilder } from '../data-builders';
+
+describe('Email [value object]', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should throw an error if email value is not a valid', () => {
+    const value = EmailDataBuilder.aEmail().withInvalidEmail().build();
+
+    expect(() => new Email(value)).toThrow(
+      'O endereço de e-mail fornecido não é válido.',
+    );
+  });
+
+  it('should throw an error if email does not have a domain', () => {
+    const value = EmailDataBuilder.aEmail().withNotUser().build();
+
+    expect(() => new Email(value)).toThrow(
+      'O endereço de e-mail fornecido não é válido.',
+    );
+  });
+
+  it('should throw an error if email has blank spaces', () => {
+    const value = EmailDataBuilder.aEmail().withSpaces().build();
+
+    expect(() => new Email(value)).toThrow(
+      'O endereço de e-mail fornecido não é válido.',
+    );
+  });
+
+  it('should throw an error if email point incorrectly', () => {
+    const value = EmailDataBuilder.aEmail().withPointIncorrectly().build();
+
+    expect(() => new Email(value)).toThrow(
+      'O endereço de e-mail fornecido não é válido.',
+    );
+  });
+
+  it('should throw an error if email have multiple @', () => {
+    const value = EmailDataBuilder.aEmail().withMultipleAt().build();
+
+    expect(() => new Email(value)).toThrow(
+      'O endereço de e-mail fornecido não é válido.',
+    );
+  });
+
+  it('should be create an email with success', () => {
+    const value = EmailDataBuilder.aEmail().build();
+    const email = new Email(value);
+
+    expect(email).toBeInstanceOf(Email);
+    expect(email.value).toBe('john@mail.com');
+  });
+});
